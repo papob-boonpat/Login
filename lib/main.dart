67607ad90281fdc0b48f22fca5a5ddfe13fab5a5
papob-boonpat <boonpat.papob@gmail.com>
2021-09-06 13:23:37 +0700

    get started page

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _LoginPageState extends State<LoginPage> {
                       Container(
                         margin: EdgeInsets.all(20),
                         child: Text(
-                          "Login App",
+                          "Logins App",
                           style: TextStyle(color: _headingColor, fontSize: 16),
                         ),
                       ),
